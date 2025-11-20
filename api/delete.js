2780cf00a21fcd0e55210108948ce06f7ff6a13b
Iblameportacio <gm.portacio@gmail.com>
2025-11-19 23:32:31 -0500

    Refactor delete handler to use 'path' parameter
    Updated the query parameter from 'pathname' to 'path' and changed the environment variable name to 'VERCEL_BLOB_READ_WRITE_TOKEN'. Added error logging.

--- api/delete.js
+++ api/delete.js
@@ -2,14 +2,15 @@ import { del } from "@vercel/blob";
 
 export default async function handler(req, res) {
   try {
-    const { pathname } = req.query;
+    const { path } = req.query;
 
-    await del(pathname, {
-      token: process.env.BLOB_READ_WRITE_TOKEN
+    await del(path, {
+      token: process.env.VERCEL_BLOB_READ_WRITE_TOKEN
     });
 
     res.status(200).json({ ok: true });
   } catch (err) {
+    console.error("DELETE ERROR:", err);
     res.status(500).json({ error: err.toString() });
   }
 }
