a8cc53e727e20cc2ca612cda383d30bf1612126b
Iblameportacio <gm.portacio@gmail.com>
2025-11-19 22:39:00 -0500

    Return blobs property in API response
    Update response to return blobs property from list.

--- api/list.js
+++ api/list.js
@@ -1,6 +1,6 @@
-import { list } from '@vercel/blob';
+import { list } from "@vercel/blob";
 
 export default async function handler(req, res) {
   const blobs = await list();
-  res.status(200).json(blobs);
+  res.status(200).json(blobs.blobs);
 }
