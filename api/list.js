3a88673d749d060c810f3010268234ca21c4bf43
Iblameportacio <gm.portacio@gmail.com>
2025-11-19 23:32:51 -0500

    Update token environment variable for blob listing

--- api/list.js
+++ api/list.js
@@ -3,7 +3,7 @@ import { list } from "@vercel/blob";
 export default async function handler(req, res) {
   try {
     const { blobs } = await list({
-      token: process.env.BLOB_READ_WRITE_TOKEN,
+      token: process.env.VERCEL_BLOB_READ_WRITE_TOKEN,
     });
 
     res.status(200).json(blobs);
